900c354b151e73a88d023b2a41dd07cd4b9c3aaf
Jacob Huempfner <jacobrh94@hotmail.com>
2019-12-06 00:30:31 -0600

    Reorganize ThirdPartyCredits window, update UiCore
    Reorganized ThirdPartyCredits window to use expanders rather than a (rather ugly-looking) list. Updated UiCore to improve LinkTextBlock control, and to change version to "1.4.1".

--- PathfinderJson/ThirdPartyCredits.xaml.cs
+++ PathfinderJson/ThirdPartyCredits.xaml.cs
@@ -18,6 +18,26 @@ namespace PathfinderJson
             InitializeComponent();
             ApplyColorScheme(App.ColorScheme);
             btnClose.ApplyColorScheme(App.ColorScheme);
+
+            expUiCore.Background = App.ColorScheme.LightBackgroundColor.ToBrush();
+            expUiCore.BorderBrush = App.ColorScheme.ThirdHighlightColor.ToBrush();
+            expUiCore.BorderThickness = new Thickness(1);
+
+            expJson.Background = App.ColorScheme.LightBackgroundColor.ToBrush();
+            expJson.BorderBrush = App.ColorScheme.ThirdHighlightColor.ToBrush();
+            expJson.BorderThickness = new Thickness(1);
+
+            expAvalon.Background = App.ColorScheme.LightBackgroundColor.ToBrush();
+            expAvalon.BorderBrush = App.ColorScheme.ThirdHighlightColor.ToBrush();
+            expAvalon.BorderThickness = new Thickness(1);
+
+            expMark1.Background = App.ColorScheme.LightBackgroundColor.ToBrush();
+            expMark1.BorderBrush = App.ColorScheme.ThirdHighlightColor.ToBrush();
+            expMark1.BorderThickness = new Thickness(1);
+
+            expMark2.Background = App.ColorScheme.LightBackgroundColor.ToBrush();
+            expMark2.BorderBrush = App.ColorScheme.ThirdHighlightColor.ToBrush();
+            expMark2.BorderThickness = new Thickness(1);
         }
 
         private void btnClose_Click(object sender, EventArgs e)
