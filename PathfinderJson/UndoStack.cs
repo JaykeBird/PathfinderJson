e5a1644d23517062b6b70e4432f8ea40532cd7cb
JaykeBird <jacobrh94@hotmail.com>
2021-05-26 00:59:36 -0500

    Create flexible UndoStack class
    This is my own personal UndoStack class that I built, that I'll use here. It uses a generic type, so any item can be stored and used for undoing/redoing. It is, indeed, a state-based undo system. Also created a barebones window to test out the feature, and after a few bug fixes, it seems to be working good. Next step is getting it set up with the MainWindow itself!

---
+++ PathfinderJson/UndoStack.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfinderJson
+{
+    public class UndoStack<T>
+    {
+        private uint undoSize = 30;
+        private List<T> undoList = new List<T>();
+        private List<T> redoList = new List<T>();
+
+        public int UndoCount { get => undoList.Count; }
+        public int RedoCount { get => redoList.Count; }
+
+        public uint UndoSizeLimit
+        {
+            get
+            {
+                return undoSize;
+            }
+            set
+            {
+                uint oldSize = undoSize;
+                undoSize = value;
+                if (undoSize < oldSize)
+                {
+                    while (undoList.Count > undoSize)
+                    {
+                        undoList.RemoveAt(0);
+                    }
+                }
+            }
+        }
+
+        public bool CanUndo
+        {
+            get
+            {
+                if (SkipCurrentStateOnUndo && !isUndoing) return undoList.Count > 1;
+                else return undoList.Count > 0;
+            }
+        }
+        public bool CanRedo
+        {
+            get
+            {
+                if (SkipCurrentStateOnUndo && !isRedoing) return redoList.Count > 1;
+                else return redoList.Count > 0;
+            }
+        }
+
+        bool isUndoing = false;
+        bool isRedoing = false;
+
+        public void StoreState(T state)
+        {
+            undoList.Add(state);
+
+            while (undoList.Count > undoSize)
+            {
+                undoList.RemoveAt(0);
+            }
+
+            redoList.Clear();
+            isUndoing = false;
+            isRedoing = false;
+        }
+
+        public T PeekUndo()
+        {
+            if (undoList.Count > 0)
+            {
+                return undoList[^1];
+            }
+            else
+            {
+                throw new InvalidOperationException("There are no available undo states.");
+            }
+        }
+
+        public T PeekRedo()
+        {
+            if (redoList.Count > 0)
+            {
+                return redoList[^1];
+            }
+            else
+            {
+                throw new InvalidOperationException("There are no available redo states.");
+            }
+        }
+
+        public bool SkipCurrentStateOnUndo { get; set; } = true;
+
+        public T Undo()
+        {
+            return Undo(true);
+        }
+
+        public T Undo(bool skipCurrentState)
+        {
+            if (undoList.Count > 0)
+            {
+                if ((SkipCurrentStateOnUndo || skipCurrentState) && !isUndoing)
+                {
+                    T pval = undoList[^1];
+                    redoList.Add(pval);
+                    undoList.RemoveAt(undoList.Count - 1);
+                    if (undoList.Count > 0)
+                    {
+                        T value = undoList[^1];
+                        redoList.Add(value);
+                        undoList.RemoveAt(undoList.Count - 1);
+                        isUndoing = true;
+                        isRedoing = false;
+                        return value;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("There are no available undo states.");
+                    }
+                }
+                else
+                {
+                    T value = undoList[^1];
+                    redoList.Add(value);
+                    undoList.RemoveAt(undoList.Count - 1);
+                    isUndoing = true;
+                    isRedoing = false;
+                    return value;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("There are no available undo states.");
+            }
+        }
+
+        public T Redo()
+        {
+            return Redo(true);
+        }
+
+        public T Redo(bool skipCurrentState)
+        {
+            if (redoList.Count > 0)
+            {
+                if ((SkipCurrentStateOnUndo || skipCurrentState) && !isRedoing)
+                {
+                    T pval = redoList[^1];
+                    undoList.Add(pval);
+                    redoList.RemoveAt(redoList.Count - 1);
+                    if (redoList.Count > 0)
+                    {
+                        T value = redoList[^1];
+                        undoList.Add(value);
+                        redoList.RemoveAt(redoList.Count - 1);
+                        isRedoing = true;
+                        isUndoing = false;
+                        return value;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("There are no available redo states.");
+                    }
+                }
+                else
+                {
+                    T value = redoList[^1];
+                    undoList.Add(value);
+                    redoList.RemoveAt(redoList.Count - 1);
+                    isRedoing = true;
+                    isUndoing = false;
+                    return value;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("There are no available redo states.");
+            }
+        }
+
+    }
+}
---
+++ PathfinderJson/UndoStackTest.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PathfinderJson
+{
+    /// <summary>
+    /// Interaction logic for UndoStackTest.xaml
+    /// </summary>
+    public partial class UndoStackTest : Window
+    {
+        public UndoStackTest()
+        {
+            InitializeComponent();
+        }
+
+        UndoStack<string> undos = new UndoStack<string>();
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // store state
+            undos.StoreState(textBox.Text);
+
+            UpdateStatusLabels();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // undo
+            if (undos.CanUndo)
+            {
+                textBox.Text = undos.Undo();
+                UpdateStatusLabels("undo");
+            }
+            else
+            {
+                UpdateStatusLabels("can't undo");
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            // redo
+            if (undos.CanRedo)
+            {
+                textBox.Text = undos.Redo();
+                UpdateStatusLabels("redo");
+            }
+            else
+            {
+                UpdateStatusLabels("can't redo");
+            }
+        }
+
+        private void UpdateStatusLabels(string lastAction = "updateState")
+        {
+            tb1.Text = undos.UndoCount.ToString();
+            tb2.Text = undos.RedoCount.ToString();
+            if (undos.UndoCount > 0)
+            {
+                tb3.Text = undos.PeekUndo();
+            }
+            else
+            {
+                tb3.Text = "-";
+            }
+            tb4.Text = lastAction;
+        }
+    }
+}
