1f0c3db3316cbe30bed8c41cccd23462aa299cb4
JaykeBird <jacobrh94@hotmail.com>
2021-07-07 21:27:34 -0500

    Added Skill Lists to PathfinderJson
    Added skill list support to PathfinderJson, similar to how it is in SentinelsJson. The biggest difference is that PathfinderJson utilizes a special SkillEditorFactory class; for right now, I decided to simply modify the SkillEditorFactory to use skill lists rather than recreate the entire skill system used in SentinelsJson.
    
    The SkillEditorFactory will by default load the skill list included in PathfinderJson with the standard skills, but if a sheet has a specific skill list file listed, the SkillEditorFactory can find it and use it instead.
    
    With this, custom skill lists can be used just like in SentinelsJson. This opens the door for customization where otherwise not possible (such as with third-party classes that add extra skills). A proper Skill List editor will come to both PathfinderJson and SentinelsJson soon.

---
+++ PathfinderJson/SkillList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PathfinderJson
+{
+    public class SkillList
+    {
+
+        public static SkillList LoadStandardList()
+        {
+            //var ss = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+            return Load(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("PathfinderJson.SkillList.pathfinder.json"), throwOnNull: false);
+            //return Load(Application.GetResourceStream(new Uri("pack://application:,,,/SkillLists/pathfinder.json")).Stream);
+        }
+
+        public static SkillList Load(Stream? s, string filename = "", bool throwOnNull = true)
+        {
+            if (s == null)
+            {
+                if (throwOnNull) throw new ArgumentNullException(nameof(s), "Stream cannot be null. Function can be set to return an empty list if \"throwOnNull\" is set to false.");
+                else return new SkillList();
+            }
+
+            try
+            {
+                using StreamReader file = new StreamReader(s);
+                JsonSerializer serializer = new JsonSerializer();
+
+                SkillList? ss = serializer.Deserialize<SkillList>(new JsonTextReader(file));
+                if (ss == null) ss = new SkillList();
+                return ss;
+            }
+            catch (JsonReaderException e)
+            {
+                throw new FormatException("This file does not match the format of a skill list JSON file.", e);
+                //MessageBox.Show("The SkillList file for SentinelsJson was corrupted. SentinelsJson will continue with default SkillList.",
+                //    "SkillList Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //SkillList sn = new SkillList();
+                //sn.Save(filename); // exception handling not needed for these as calling function handles exceptions
+                //return sn;
+            }
+            catch (FileNotFoundException)
+            {
+                throw;
+                //SkillList sn = new SkillList();
+                //sn.Save(filename); // exception handling not needed for these as calling function handles exceptions
+                //return sn;
+            }
+        }
+
+        public static SkillList LoadFile(string filename)
+        {
+            return Load(File.OpenRead(filename));
+        }
+
+        public void Save(string filename)
+        {
+            DirectoryInfo di = Directory.GetParent(filename);
+
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+
+            using StreamWriter file = new StreamWriter(filename, false, new UTF8Encoding(false));
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(file, this);
+        }
+
+        public string Name { get; set; } = "Unnamed";
+
+        public string GameSystem { get; set; } = "Pathfinder1e";
+
+        [JsonProperty("skills")]
+        public List<SkillListEntry> SkillEntries { get; private set; } = new List<SkillListEntry>();
+
+        public void AddSkill(SkillListEntry sle)
+        {
+            SkillEntries.Add(sle);
+        }
+    }
+
+    public class SkillListEntry
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Include)]
+        public string Name { get; set; } = "Unnamed";
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string? DisplayName { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Include)]
+        public string Modifier { get; set; } = "INT";
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string? InfoUrl { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool HasSpecialization { get; set; } = false;
+    }
+}
