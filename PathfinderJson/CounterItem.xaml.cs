ebe511c5444624fe58f24fc70d499c8b8796d092
JaykeBird <jacobrh94@hotmail.com>
2022-02-18 09:38:51 -0600

    Expand edit mode functions of CounterItem
    When you enter edit mode now, you can change not only the counter title, but also the color and you can directly set the value as well!
    
    I may in the future streamline this even further, but for here and now, it'll work. One fix I do need to make is updating the icons for edit mode: currently Save and Undo are used (for "apply" and "cancel" respectively), but I want to instead replace them with a Check icon and either Cancel or Back.

--- PathfinderJson/CounterItem.xaml.cs
+++ PathfinderJson/CounterItem.xaml.cs
@@ -26,7 +26,9 @@ namespace PathfinderJson
             InitializeComponent();
         }
 
-        public int Value { get; set; }
+        private int _value = 0;
+
+        public int Value { get => _value; set { _value = value; lblValue.Text = value.ToString(); } }
 
         #region Color Scheme
 
@@ -63,16 +65,51 @@ namespace PathfinderJson
         }
         #endregion
 
+        bool editMode = false;
+
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-            RemoveRequested?.Invoke(this, EventArgs.Empty);
+            if (editMode)
+            {
+                ExitEditMode();
+            }
+            else
+            {
+                RemoveRequested?.Invoke(this, EventArgs.Empty);
+            }
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
-            txtTitle.Visibility = Visibility.Visible;
-            txtTitle.Text = lblTitle.Text;
-            txtTitle.Focus();
+            if (editMode)
+            {
+                ApplyEditChanges();
+                ExitEditMode();
+            }
+            else
+            {
+                nudInput.Visibility = Visibility.Visible;
+                nudInput.Value = Value;
+                btnChangeColor.Visibility = Visibility.Visible;
+                btnChangeColor.Background = brdrEllipse.Background;
+                btnChangeColor.HighlightBrush = brdrEllipse.Background;
+                btnChangeColor.ClickBrush = brdrEllipse.Background;
+
+                txtTitle.Visibility = Visibility.Visible;
+                txtTitle.Text = lblTitle.Text;
+                txtTitle.Focus();
+
+                btnDecrease.Visibility = Visibility.Collapsed;
+                btnIncrease.Visibility = Visibility.Collapsed;
+
+                imgEdit.ImageName = "Save";
+                imgDelete.ImageName = "Undo";
+
+                btnEdit.ToolTip = "Apply Changes";
+                btnDelete.ToolTip = "Cancel";
+
+                editMode = true;
+            }
         }
 
         private void btnDecrease_Click(object sender, RoutedEventArgs e)
@@ -85,7 +122,7 @@ namespace PathfinderJson
             {
                 Value--;
             }
-            lblValue.Text = Value.ToString();
+            //lblValue.Text = Value.ToString();
         }
 
         private void btnIncrease_Click(object sender, RoutedEventArgs e)
@@ -98,15 +135,62 @@ namespace PathfinderJson
             {
                 Value++;
             }
-            lblValue.Text = Value.ToString();
+            //lblValue.Text = Value.ToString();
         }
 
         private void txtTitle_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                txtTitle.Visibility = Visibility.Collapsed;
-                lblTitle.Text = txtTitle.Text;
+                ApplyEditChanges();
+                ExitEditMode();
+            }
+            else if (e.Key == Key.Escape)
+            {
+                ExitEditMode();
+            }
+        }
+
+        void ApplyEditChanges()
+        {
+            lblTitle.Text = txtTitle.Text;
+            Value = nudInput.Value;
+            if (brdrEllipse.Background != btnChangeColor.Background)
+            {
+                brdrEllipse.Background = btnChangeColor.Background;
+                innerBorder.Background = btnChangeColor.Background;
+            }
+        }
+
+        void ExitEditMode()
+        {
+            nudInput.Visibility = Visibility.Collapsed;
+            btnChangeColor.Visibility = Visibility.Collapsed;
+            txtTitle.Visibility = Visibility.Collapsed;
+
+            btnDecrease.Visibility = Visibility.Visible;
+            btnIncrease.Visibility = Visibility.Visible;
+
+            imgEdit.ImageName = "Edit";
+            imgDelete.ImageName = "Cancel";
+
+            btnEdit.ToolTip = "Edit";
+            btnDelete.ToolTip = "Delete";
+
+            editMode = false;
+        }
+
+        private void btnChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            ColorPickerDialog cpd = new ColorPickerDialog(ColorScheme, ((SolidColorBrush)btnChangeColor.Background).Color);
+            cpd.Owner = Window.GetWindow(this);
+            cpd.ShowDialog();
+
+            if (cpd.DialogResult)
+            {
+                btnChangeColor.Background = cpd.SelectedColor.ToBrush();
+                btnChangeColor.HighlightBrush = cpd.SelectedColor.ToBrush();
+                btnChangeColor.ClickBrush = cpd.SelectedColor.ToBrush();
             }
         }
     }
