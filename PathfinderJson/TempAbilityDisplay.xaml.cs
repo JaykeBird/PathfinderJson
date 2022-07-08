adaaa88c0c2e5fded551feec87bd444e8e8be893
JaykeBird <jacobrh94@hotmail.com>
2022-07-08 00:35:12 -0500

    Add visual editor for temporary ability scores
    After deciding prior to version 1.0 to not visually support temporary ability scores from Mottokrosh's site, I've gone back and implemented them now!
    
    My implementation isn't 100% like how Mottokrosh's probably is; you have to click on the "+Temp" button to actually display and set a temporary value for a given ability score.
    
    Anyway, though, this loads them in from Mottokrosh's site just fine.
    
    I've done a couple of things to try to minimize repetitive code (and not tank performance) while loading in a character sheet, but we'll see how much that helps after some future commits.
    
    I think I may need to take another look at how I load character sheets...

---
+++ PathfinderJson/TempAbilityDisplay.xaml.cs
@@ -0,0 +1,79 @@
+﻿using SolidShineUi;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PathfinderJson
+{
+    /// <summary>
+    /// Interaction logic for TempAbilityDisplay.xaml
+    /// </summary>
+    public partial class TempAbilityDisplay : UserControl
+    {
+        public TempAbilityDisplay()
+        {
+            InitializeComponent();
+        }
+
+        #region ColorScheme
+
+        public event DependencyPropertyChangedEventHandler? ColorSchemeChanged;
+
+        public static DependencyProperty ColorSchemeProperty
+            = DependencyProperty.Register("ColorScheme", typeof(ColorScheme), typeof(TempAbilityDisplay),
+            new FrameworkPropertyMetadata(new ColorScheme(), new PropertyChangedCallback(OnColorSchemeChanged)));
+
+        public static void OnColorSchemeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ColorScheme cs = (e.NewValue as ColorScheme)!;
+
+            if (d is TempAbilityDisplay s)
+            {
+                s.ColorSchemeChanged?.Invoke(d, e);
+                s.ApplyColorScheme(cs);
+            }
+        }
+
+        public ColorScheme ColorScheme
+        {
+            get => (ColorScheme)GetValue(ColorSchemeProperty);
+            set => SetValue(ColorSchemeProperty, value);
+        }
+
+        public void ApplyColorScheme(ColorScheme cs)
+        {
+            if (cs != ColorScheme)
+            {
+                ColorScheme = cs;
+                return;
+            }
+
+            brdr.BorderBrush = cs.SecondaryColor.ToBrush();
+            brdr.Background = cs.LightBackgroundColor.ToBrush();
+        }
+
+        #endregion
+
+        public event EventHandler? CloseRequested;
+
+        public int Value { get => (int?)nudValue.Value ?? 0; set => nudValue.Value = value; }
+
+        int _modifier = 0;
+
+        public int Modifier { get => _modifier; }
+
+        private void nudValue_ValueChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            _modifier = CoreUtils.CalculateModifierInt((int)e.NewValue);
+
+            if (txtMod == null) return;
+
+            txtMod.Text = "( " + CoreUtils.DisplayModifier(_modifier) + " )";
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            CloseRequested?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
