6a0fdab298da0cbafa2dbc0766de10afcccf9735
JaykeBird <jacobrh94@hotmail.com>
2022-08-15 01:06:35 -0500

    Create ColorSchemeDialog to change color scheme
    This will replace the various functions currently in place to allow users to change the color scheme. Note that the dialog isn't fully complete, and there's other further work to be done to fully integrate it into the UI. Also note that some changes will not be preserved when the program is closed and re-opened. But at least now a bulk of the visual work is done, now it's just a lot of backend stuff.
    
    Also fixed a few issues in relation to the ItemEditor not supporting the Quantity value as a string.

---
+++ PathfinderJson/ChangeTheme/ColorChangeButton.xaml.cs
@@ -0,0 +1,48 @@
+﻿using SolidShineUi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PathfinderJson.ChangeTheme
+{
+    /// <summary>
+    /// Interaction logic for ColorChangeButton.xaml
+    /// </summary>
+    public partial class ColorChangeButton : UserControl
+    {
+        public ColorChangeButton()
+        {
+            InitializeComponent();
+            fb.Click += (s, e) => { Click?.Invoke(this, EventArgs.Empty); };
+        }
+
+        public event EventHandler? Click;
+
+        public Color DisplayedColor { get => (Color)GetValue(DisplayedColorProperty); set => SetValue(DisplayedColorProperty, value); }
+
+        public static DependencyProperty DisplayedColorProperty
+            = DependencyProperty.Register("DisplayedColor", typeof(Color), typeof(ColorChangeButton));
+
+        public ColorScheme ColorScheme { get => (ColorScheme)GetValue(ColorSchemeProperty); set => SetValue(ColorSchemeProperty, value); }
+
+        public static DependencyProperty ColorSchemeProperty
+            = DependencyProperty.Register("ColorScheme", typeof(ColorScheme), typeof(ColorChangeButton));
+
+        public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+
+        public static DependencyProperty TitleProperty
+            = DependencyProperty.Register("Title", typeof(string), typeof(ColorChangeButton));
+
+        public bool ShowTitle { get => (bool)GetValue(ShowTitleProperty); set => SetValue(ShowTitleProperty, value); }
+
+        public static DependencyProperty ShowTitleProperty
+            = DependencyProperty.Register("ShowTitle", typeof(bool), typeof(ColorChangeButton),
+            new FrameworkPropertyMetadata(false));
+
+    }
+}
---
+++ PathfinderJson/ChangeTheme/ColorSchemeButton.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SolidShineUi;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace PathfinderJson.ChangeTheme
+{
+    /// <summary>
+    /// Interaction logic for ColorSchemeButton.xaml
+    /// </summary>
+    public partial class ColorSchemeButton : UserControl
+    {
+        public ColorSchemeButton()
+        {
+            InitializeComponent();
+            btn.Click += (s, e) => { Click?.Invoke(this, EventArgs.Empty); };
+        }
+
+        public void PerformClick()
+        {
+            Click?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler? Click;
+
+        private ColorScheme _cs = new ColorScheme();
+
+        public ColorScheme DisplayedColorScheme
+        {
+            get { return _cs; }
+            set
+            {
+                _cs = value;
+
+                brdrWindow.BorderBrush = _cs.BorderColor.ToBrush();
+                brdrWindow.Background = _cs.BackgroundColor.ToBrush();
+                txtFore.Foreground = _cs.ForegroundColor.ToBrush();
+                grdTitlebar.Background = _cs.WindowTitleBarColor.ToBrush();
+                grdMain.Background = _cs.MainColor.ToBrush();
+                brdrPopout.BorderBrush = _cs.BorderColor.ToBrush();
+                brdrPopout.Background = _cs.SecondaryColor.ToBrush();
+            }
+        }
+
+        public ColorScheme ColorScheme { get => (ColorScheme)GetValue(ColorSchemeProperty); set => SetValue(ColorSchemeProperty, value); }
+
+        public static DependencyProperty ColorSchemeProperty
+            = DependencyProperty.Register("ColorScheme", typeof(ColorScheme), typeof(ColorSchemeButton));
+
+        public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+
+        public static DependencyProperty TitleProperty
+            = DependencyProperty.Register("Title", typeof(string), typeof(ColorSchemeButton));
+
+        public bool ShowTitle { get => (bool)GetValue(ShowTitleProperty); set => SetValue(ShowTitleProperty, value); }
+
+        public static DependencyProperty ShowTitleProperty
+            = DependencyProperty.Register("ShowTitle", typeof(bool), typeof(ColorSchemeButton),
+            new FrameworkPropertyMetadata(false));
+
+        public bool IsSelected { get => (bool)GetValue(IsSelectedProperty); set => SetValue(IsSelectedProperty, value); }
+
+        public static DependencyProperty IsSelectedProperty
+            = DependencyProperty.Register("IsSelected", typeof(bool), typeof(ColorSchemeButton),
+            new FrameworkPropertyMetadata(false));
+
+
+        public static ColorSchemeButton CreateButtonFromScheme(ColorScheme cs, string? title = null)
+        {
+            ColorSchemeButton csb = new ColorSchemeButton();
+            csb.DisplayedColorScheme = cs;
+            csb.Title = title ?? "";
+            csb.ShowTitle = !(title == null);
+
+            return csb;
+        }
+
+    }
+}
