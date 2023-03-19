8b581066103793521632e0b77fd1075bc8740056
JaykeBird <jacobrh94@hotmail.com>
2023-03-18 23:21:56 -0500

    Fix recent file menu bug when submenu is enabled
    When the submenu option is enabled for the recent files menu, trying to select any option off the submenu also ends up loading the file in the current instance, which is not desirable. Now, an extra check is done to make sure this doesn't happen.

--- PathfinderJson/MainWindow.xaml.cs
+++ PathfinderJson/MainWindow.xaml.cs
@@ -952,7 +952,7 @@ namespace PathfinderJson
             };
             mi.ToolTip = tt;
             mi.Tag = filename;
-            mi.Click += miRecentFile_Click;
+            mi.Click += MainRecentFileClick;
             mi.ContextMenuOpening += miRecentContext_Opening;
             mnuRecent.Items.Insert(0, mi);
 
@@ -1008,6 +1008,13 @@ namespace PathfinderJson
                     cm.ApplyColorScheme(App.ColorScheme);
                 }
             }
+        }
+
+        private void MainRecentFileClick(object sender, RoutedEventArgs e)
+        {
+            if (App.Settings.DisplayRecentActionsAsSubmenu) return;
+
+            miRecentFile_Click(sender, e);
         }
 
         private void miRecentFile_Click(object sender, RoutedEventArgs e)
