f3e3f30633bcad0d134c57f52bb84f59f7ae7949
JaykeBird <jacobrh94@hotmail.com>
2023-04-03 09:24:14 -0500

    Added quick view change menu in bottom-left corner
    Added a menu in the bottom-left corner to allow users to quickly change the view, rather than using the View menu. This is also slightly closer to the look of the online sheet editor.

--- PathfinderJson/MainWindow.xaml.cs
+++ PathfinderJson/MainWindow.xaml.cs
@@ -1761,6 +1761,13 @@ namespace PathfinderJson
                     mnuScroll.IsChecked = true;
                     mnuRawJson.IsChecked = false;
 
+                    mnuQTabs.IsChecked = false;
+                    mnuQScroll.IsChecked = true;
+                    mnuQRawJson.IsChecked = false;
+
+                    imgQView.ImageName = "ContinuousScroll";
+                    txtQViewName.Text = "Scroll View";
+
                     if (_isEditorDirty && updateSheet)
                     {
                         // update sheet from editor
@@ -1783,6 +1790,13 @@ namespace PathfinderJson
                     mnuScroll.IsChecked = false;
                     mnuRawJson.IsChecked = false;
 
+                    mnuQTabs.IsChecked = true;
+                    mnuQScroll.IsChecked = false;
+                    mnuQRawJson.IsChecked = false;
+
+                    imgQView.ImageName = "TabView";
+                    txtQViewName.Text = "Tabbed View";
+
                     if (_isEditorDirty && updateSheet)
                     {
                         // update sheet from editor
@@ -1801,6 +1815,13 @@ namespace PathfinderJson
                     mnuTabs.IsChecked = false;
                     mnuScroll.IsChecked = false;
                     mnuRawJson.IsChecked = true;
+
+                    mnuQTabs.IsChecked = false;
+                    mnuQScroll.IsChecked = false;
+                    mnuQRawJson.IsChecked = true;
+
+                    imgQView.ImageName = "TextFile";
+                    txtQViewName.Text = "Raw JSON View";
 
                     if (_isTabsDirty && updateSheet)
                     {
