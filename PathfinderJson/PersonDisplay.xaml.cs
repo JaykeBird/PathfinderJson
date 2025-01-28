84efe3bb93a47ca2e5cf6847e7871442cee453ba
JaykeBird <jacobrh94@hotmail.com>
2025-01-27 23:03:34 -0600

    Add in Person Display contol (does nothing yet)
    This is the beginnings of the last major feature for 1.3: the person display. This will be used to visually indicate where you have your items located and stored, and ideally also will be able to choose to equip and unequip them (I'll add on equiping UI to this and the item editor later on).
    
    Right now, this is purely visual demonstration to help me determine how I want things to look; future commits will have me actually hooking up the side panes to actual data-driven items.
    
    This is going to be an ongoing project for a bit... 1.3 will still be a bit of a ways away from now.

---
+++ PathfinderJson/PersonDisplay.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using SolidShineUi;
+
+namespace PathfinderJson
+{
+    /// <summary>
+    /// Interaction logic for PersonDisplay.xaml
+    /// </summary>
+    public partial class PersonDisplay : UserControl
+    {
+        public PersonDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public ColorScheme ColorScheme { get => (ColorScheme)GetValue(ColorSchemeProperty); set => SetValue(ColorSchemeProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="ColorScheme"/>. See the related property for details.</summary>
+        public static DependencyProperty ColorSchemeProperty
+            = DependencyProperty.Register(nameof(ColorScheme), typeof(ColorScheme), typeof(PersonDisplay),
+            new FrameworkPropertyMetadata(new ColorScheme()));
+
+    }
+}
