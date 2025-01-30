78725e32b3b192462115b1caac76cc6d760d31ae
JaykeBird <jacobrh94@hotmail.com>
2025-01-29 22:12:26 -0600

    Created new controls to use with PersonDisplay
    Thte PersonSlotItemList and SmalltemDisplay will be used with the PersonDisplay control. These will be the ways that I get the data into the control to actually properly display the items on the character sheet.
    
    Still some progress to go, primarily in actually building the SmallItemDisplay and then hooking everything up in the PersonDisplay, but this is a major step forward.

---
+++ PathfinderJson/PersonSlotItemList.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using SolidShineUi;
+
+namespace PathfinderJson
+{
+    /// <summary>
+    /// A control that can list a collection of items in a person slot.
+    /// </summary>
+    public partial class PersonSlotItemList : UserControl
+    {
+        public PersonSlotItemList()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The color used for this slot, to help users with identifying the slot.
+        /// </summary>
+        public Color SlotColor { get => (Color)GetValue(SlotColorProperty); set => SetValue(SlotColorProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="SlotColor"/>. See the related property for details.</summary>
+        public static DependencyProperty SlotColorProperty
+            = DependencyProperty.Register(nameof(SlotColor), typeof(Color), typeof(PersonSlotItemList),
+            new FrameworkPropertyMetadata(Colors.White));
+
+        /// <summary>
+        /// The title of the slot.
+        /// </summary>
+        public string Title { get => (string)GetValue(SlotTitleProperty); set => SetValue(SlotTitleProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="Title"/>. See the related property for details.</summary>
+        public static DependencyProperty SlotTitleProperty
+            = DependencyProperty.Register(nameof(Title), typeof(string), typeof(PersonSlotItemList),
+            new FrameworkPropertyMetadata("None"));
+
+        /// <summary>
+        /// A collection of the editors for the equipment that is currently in this slot.
+        /// </summary>
+        public SelectableCollection<SmallItemDisplay> EquipmentEditors { get => (SelectableCollection<SmallItemDisplay>)GetValue(EquipmentEditorsProperty); set => SetValue(EquipmentEditorsProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="EquipmentEditors"/>. See the related property for details.</summary>
+        public static DependencyProperty EquipmentEditorsProperty
+            = DependencyProperty.Register(nameof(EquipmentEditors), typeof(SelectableCollection<SmallItemDisplay>), typeof(PersonSlotItemList),
+            new FrameworkPropertyMetadata(new SelectableCollection<SmallItemDisplay>()));
+
+
+
+    }
+}
---
+++ PathfinderJson/SmallItemDisplay.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SolidShineUi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PathfinderJson
+{
+    /// <summary>
+    /// A smaller display for <see cref="Equipment"/>, with less editing options than <see cref="ItemEditor"/>.
+    /// </summary>
+    public partial class SmallItemDisplay : SelectableUserControl
+    {
+        public SmallItemDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
