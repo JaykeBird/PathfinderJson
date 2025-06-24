87016994226de72ae10207ccbcdafbd42edaf8cb
JaykeBird <jacobrh94@hotmail.com>
2025-06-23 23:00:54 -0500

    Begin process of converting SpellEditor for ILD
    Slowly beginning the process of converting the SpellEditor to have it work in the ILD. I'm only like partway done right now, but I've been sitting on this half-done code for over a week, so may as well commit it now to have something, and I'll convert the rest at a later time.

--- PathfinderJson/SpellEditor.xaml.cs
+++ PathfinderJson/SpellEditor.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using PathfinderJson.Ild;
 using SolidShineUi;
 using static PathfinderJson.CoreUtils;
 
@@ -11,7 +12,7 @@ namespace PathfinderJson
     /// <summary>
     /// Interaction logic for SpellEditor.xaml
     /// </summary>
-    public partial class SpellEditor : SelectableUserControl
+    public partial class SpellEditor : SelectableListItem
     {
         public SpellEditor()
         {
@@ -23,11 +24,22 @@ namespace PathfinderJson
             nudLevel.ApplyColorScheme(cs);
             nudCast.ApplyColorScheme(cs);
             nudPrepared.ApplyColorScheme(cs);
+            chkMarked.ApplyColorScheme(cs);
+            expander.ApplyColorScheme(cs);
+            btnMoveDown.ApplyColorScheme(cs);
+            btnMoveUp.ApplyColorScheme(cs);
+            btnRemove.ApplyColorScheme(cs);
+            btnAddCast.ApplyColorScheme(cs);
+
+            imgMoveDown.ApplyColorScheme(cs);
+            imgMoveUp.ApplyColorScheme(cs);
+            imgRemove.ApplyColorScheme(cs);
+            imgAdd.ApplyColorScheme(cs);
         }
 
         public void LoadSpell(Spell s)
         {
-            txtName.Text = s.Name;
+            SpellName = s.Name;
             txtNotes.Text = s.Notes;
             txtSchool.Text = s.School;
             txtSubschool.Text = s.Subschool;
@@ -35,7 +47,7 @@ namespace PathfinderJson
             chkAtWill.IsChecked = s.AtWill;
             chkMarked.IsChecked = s.Marked;
 
-            nudLevel.Value = s.Level;
+            Level = s.Level;
             nudCast.Value = s.Cast;
             nudPrepared.Value = s.Prepared;
         }
@@ -44,13 +56,14 @@ namespace PathfinderJson
         {
             Spell s = new Spell
             {
-                Name = txtName.Text,
+                Name = SpellName,
                 Notes = txtNotes.Text,
                 School = txtSchool.Text,
                 Subschool = txtSubschool.Text,
-                AtWill = chkAtWill.IsChecked.GetValueOrDefault(false),
-                Marked = chkMarked.IsChecked.GetValueOrDefault(false),
-                Level = nudLevel.Value,
+                AtWill = chkAtWill.IsChecked,
+                Marked = chkMarked.IsChecked,
+                Level = Level,
+                //Level = nudLevel.Value,
                 Cast = nudCast.Value,
                 Prepared = nudPrepared.Value,
             };
@@ -70,39 +83,68 @@ namespace PathfinderJson
             }
         }
 
-        public int Level
+        [IldLink("Name")]
+        public string SpellName { get => (string)GetValue(SpellNameProperty); set => SetValue(SpellNameProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="SpellName"/>. See the related property for details.</summary>
+        public static DependencyProperty SpellNameProperty
+            = DependencyProperty.Register(nameof(SpellName), typeof(string), typeof(SpellEditor),
+            new FrameworkPropertyMetadata("", OnSpellNameChanged));
+
+        private static void OnSpellNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            get => nudLevel.Value;
-            set => nudLevel.Value = value;
+            if (d is SpellEditor o)
+            {
+                if (o.SpellName == "")
+                {
+                    o.txtName.ToolTip = "Spell Name";
+                }
+                else
+                {
+                    o.txtName.ToolTip = "Spell Name:\n" + o.SpellName;
+                }
+            }
+        }
+
+        [IldLink("Level")]
+        public int Level { get => (int)GetValue(LevelProperty); set => SetValue(LevelProperty, value); }
+
+        /// <summary>The backing dependency property for <see cref="Level"/>. See the related property for details.</summary>
+        public static DependencyProperty LevelProperty
+            = DependencyProperty.Register(nameof(Level), typeof(int), typeof(SpellEditor),
+            new FrameworkPropertyMetadata(0, OnLevelChanged));
+
+        private static void OnLevelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is SpellEditor o)
+            {
+                o.chkMarked.Content = o.Level switch
+                {
+                    1 => "1st",
+                    2 => "2nd",
+                    3 => "3rd",
+                    _ => o.Level + "th"
+                };
+            }
         }
 
         public bool Marked
         {
-            get => chkMarked.IsChecked.GetValueOrDefault(false);
+            get => chkMarked.IsChecked;
             set => chkMarked.IsChecked = value;
         }
 
-        private void Expander_Expanded(object sender, RoutedEventArgs e)
-        {
-            rowDetails.Height = new GridLength(1, GridUnitType.Star);
-        }
-
-        private void Expander_Collapsed(object sender, RoutedEventArgs e)
-        {
-            rowDetails.Height = new GridLength(0);
-        }
-
         // event just to update main window's "isDirty" value
-        public event EventHandler? ContentChanged;
+        //public event EventHandler? ContentChanged;
 
         private void textbox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ContentChanged?.Invoke(this, e);
+            DoContentChanged();
         }
 
         private void nudLevel_ValueChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            ContentChanged?.Invoke(this, EventArgs.Empty);
+            DoContentChanged();
         }
 
         private void lblSearch_Click(object sender, RoutedEventArgs e)
@@ -110,9 +152,34 @@ namespace PathfinderJson
             OpenBrowser("https://cse.google.com/cse?cx=006680642033474972217%3A6zo0hx_wle8&q=" + txtName.Text);
         }
 
-        private void nudLevel_Loaded(object sender, RoutedEventArgs e)
+        public override void LoadValues(Dictionary<IldPropertyInfo, object> properties)
         {
+            LoadValuesInternal(this, properties);
+        }
 
+        public override object? GetPropertyValue(IldPropertyInfo property)
+        {
+            return GetPropertyValueInternal(this, property.Name);
+        }
+
+        public override Dictionary<string, object> GetAllProperties()
+        {
+            return GetAllPropertiesInternal(this);
+        }
+
+        private void btnMoveUp_Click(object sender, RoutedEventArgs e)
+        {
+            DoRequestMoveUp();
+        }
+
+        private void btnMoveDown_Click(object sender, RoutedEventArgs e)
+        {
+            DoRequestMoveDown();
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            DoRequestDelete();
         }
     }
 }
